eb13ea8ba2ead6cb60a9a95fc0620d680721702f
coolsquid <snakkeflink@gmail.com>
2015-09-04 00:49:57 +0200

    Update

---
+++ src/main/java/temporalreality/launcher/util/Issues.java
@@ -0,0 +1,62 @@
+package temporalreality.launcher.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.kohsuke.github.GitHub;
+
+/**
+ * @author CoolSquid
+ */
+public class Issues {
+
+	private static final GitHub github;
+
+	public static void create(String title, Throwable t, Map<String, String> additionalInfo) {
+		if (Boolean.parseBoolean(System.getProperty("temporalreality.launcher.errorreporting", "true"))) {
+			StringWriter w = new StringWriter();
+			w.write("```");
+			w.write(System.lineSeparator());
+			t.printStackTrace(new PrintWriter(w));
+			w.write(System.lineSeparator());
+			w.write(System.lineSeparator());
+			w.write(System.lineSeparator());
+			w.write("Java version: ");
+			w.write(System.getProperty("java.version"));
+			w.write(System.lineSeparator());
+			w.write("Java vendor: ");
+			w.write(System.getProperty("java.vendor"));
+			w.write(System.lineSeparator());
+			w.write("OS: ");
+			w.write(System.getProperty("os.name"));
+			w.write(System.lineSeparator());
+			w.write("Arch: ");
+			w.write(System.getProperty("os.arch"));
+			w.write(System.lineSeparator());
+			if (additionalInfo != null)
+				for (Entry<String, String> e: additionalInfo.entrySet()) {
+					w.write(e.getKey());
+					w.write(": ");
+					w.write(e.getValue());
+					w.write(System.lineSeparator());
+				}
+			w.write("```");
+			try {
+				github.getUser("TemporalReality").getRepository("Automatic-Issue-Reporting").createIssue(title != null ? title : t.toString()).body(w.toString()).create();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	static {
+		try {
+			github = GitHub.connectUsingOAuth("e0c8e2fedeecb50ca22e67ce70e2278e5788b57a");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
