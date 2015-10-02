4681a980303c46a655a73c8e3708035a66614bcc
Shadowfacts <me@shadowfacts.net>
2015-10-01 20:41:42 -0400

    Use separate thread, reorganize some stuff, use Optional

--- src/main/java/temporalreality/launcher/util/Issues.java
+++ src/main/java/temporalreality/launcher/util/Issues.java
@@ -6,6 +6,7 @@ import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 
 import org.kohsuke.github.GHIssue;
 import org.kohsuke.github.GHIssueState;
@@ -40,61 +41,62 @@ public class Issues {
 	}
 
 	private static void create(String title, Throwable t, Map<String, String> additionalInfo) {
-		try {
-			if (Boolean.parseBoolean(System.getProperty("temporalreality.launcher.errorreporting", "true"))) {
-				title = title != null ? title : t.toString();
-				GHRepository repo = github.getUser("TemporalReality").getRepository("Automatic-Issue-Reporting");
-				GHIssue issue = getIssue(title, repo);
-				StringWriter w = new StringWriter();
-				w.write("```");
-				w.write(System.lineSeparator());
-				if (issue == null) {
-					t.printStackTrace(new PrintWriter(w));
-					w.write(System.lineSeparator());
-					w.write(System.lineSeparator());
-					w.write(System.lineSeparator());
-				}
-				w.write("Java version: ");
-				w.write(System.getProperty("java.version"));
-				w.write(System.lineSeparator());
-				w.write("Java vendor: ");
-				w.write(System.getProperty("java.vendor"));
-				w.write(System.lineSeparator());
-				w.write("OS: ");
-				w.write(System.getProperty("os.name"));
-				w.write(System.lineSeparator());
-				w.write("Arch: ");
-				w.write(System.getProperty("os.arch"));
-				w.write(System.lineSeparator());
-				if (additionalInfo != null)
-					for (Entry<String, String> e: additionalInfo.entrySet()) {
-						w.write(e.getKey());
-						w.write(": ");
-						w.write(e.getValue());
-						w.write(System.lineSeparator());
-					}
-				w.write("```");
-				if (issue == null)
-					issue = repo.createIssue(title.replace(System.getProperty("user.name"), "user")).body(w.toString().replace(System.getProperty("user.name"), "user")).create();
-				else
-					issue.comment(w.toString());
-				TRLauncher.log.catching(t);
-				TRLauncher.log.error("An error occured, and has been reported automatically. If you would like to add any information, please go to "
-						+ issue.getHtmlUrl()
-						+ '.');
-			} 
-		} catch (Throwable t2) {
-			t2.printStackTrace();
-		}
-	}
+		new Thread(() -> {
+			try {
+				if (Boolean.parseBoolean(System.getProperty("temporalreality.launcher.errorreporting", "true"))) {
+					String theTitle = title != null ? title : t.toString();
+					GHRepository repo = github.getUser("TemporalReality").getRepository("Automatic-Issue-Reporting");
+					Optional<GHIssue> optionalIssue = repo.getIssues(GHIssueState.OPEN).stream().filter(issue -> issue.getTitle().equals(theTitle)).findFirst();
+					if (optionalIssue.isPresent()) {
+						GHIssue issue = optionalIssue.get();
+						StringWriter w = new StringWriter();
 
-	private static GHIssue getIssue(String title, GHRepository repo) throws IOException {
-		for (GHIssue issue: repo.getIssues(GHIssueState.OPEN)) {
-			if (issue.getTitle().equals(title)) {
-				return issue;
+//				Stacktrace
+						if (issue == null) {
+							w.write("```");
+							t.printStackTrace(new PrintWriter(w));
+							w.write("```");
+							w.write(System.lineSeparator());
+							w.write(System.lineSeparator());
+							w.write(System.lineSeparator());
+						}
+
+//				System details
+						w.write("Java version: ");
+						w.write(System.getProperty("java.version"));
+						w.write(System.lineSeparator());
+						w.write("Java vendor: ");
+						w.write(System.getProperty("java.vendor"));
+						w.write(System.lineSeparator());
+						w.write("OS: ");
+						w.write(System.getProperty("os.name"));
+						w.write(System.lineSeparator());
+						w.write("Arch: ");
+						w.write(System.getProperty("os.arch"));
+						w.write(System.lineSeparator());
+						if (additionalInfo != null) {
+							for (Entry<String, String> e : additionalInfo.entrySet()) {
+								w.write(e.getKey());
+								w.write(": ");
+								w.write(e.getValue());
+								w.write(System.lineSeparator());
+							}
+						}
+						if (issue == null) {
+							issue = repo.createIssue(theTitle.replace(System.getProperty("user.name"), "user")).body(w.toString().replace(System.getProperty("user.name"), "user")).create();
+						} else {
+							issue.comment(w.toString().replace(System.getProperty("user.name"), "use"));
+						}
+						TRLauncher.log.catching(t);
+						TRLauncher.log.error("An error occurred, and has been reported automatically. If you would like to add any information, please go to "
+								+ issue.getHtmlUrl()
+								+ '.');
+					}
+				}
+			} catch (Throwable t2) {
+				t2.printStackTrace();
 			}
-		}
-		return null;
+		}).start();
 	}
 
 	static {
